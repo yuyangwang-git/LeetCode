f6478030fdc6cd5daf36776276ca69ba04165829
yuyangwang-git <mrwangyuyang@gmail.com>
2022-03-12 21:43:10 +0800

    add problem 605

--- 435.无重叠区间.cpp
+++ 435.无重叠区间.cpp
@@ -7,8 +7,33 @@
 // @lc code=start
 class Solution {
 public:
-    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+    static bool compare(vector<int>& interval1, vector<int>& interval2)
+    {
+        return (interval1[1] < interval2[1]);
+    }
 
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int count;
+        int pre;
+
+        sort(intervals.begin(), intervals.end(), compare);
+        
+        pre = intervals[0][1];
+        count = 0;
+        for (int i = 1; i < intervals.size(); i++)
+        {
+            if (intervals[i][0] < pre)                                    // 如果重叠
+            {
+                count++;
+            }
+            else
+            {
+                pre = intervals[i][1];                                    // 指针向后移动
+            }
+        }
+
+        printf("%d", count);
+        return count;
     }
 };
 // @lc code=end
---
+++ 605.种花问题.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=605 lang=cpp
+ *
+ * [605] 种花问题
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int count;
+        int maxFlower;
+        vector<int> bed = {1, 0};
+        bed.insert(bed.end(), flowerbed.begin(), flowerbed.end());
+        bed.push_back(0);
+        bed.push_back(1);
+
+        count = 0;
+        maxFlower = 0;
+        for (int i = 0; i < bed.size(); i++)
+        {
+            if (bed[i] == 1)
+            {
+                if (count)
+                {
+                    maxFlower += ceil(count/2.0) - 1;
+                }
+                count = 0;
+            }
+            else
+            {
+                count++;
+            }
+        }
+
+        printf("%d", maxFlower);
+        return (maxFlower >= n);
+    }
+};
+// @lc code=end
+
