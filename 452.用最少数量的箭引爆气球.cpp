7bbfd3a33fd32d7af4062aeba7819ab8920efa76
yuyangwang-git <mrwangyuyang@gmail.com>
2022-03-13 11:49:34 +0800

    add problem 763

--- 452.用最少数量的箭引爆气球.cpp
+++ 452.用最少数量的箭引爆气球.cpp
@@ -7,8 +7,33 @@
 // @lc code=start
 class Solution {
 public:
-    int findMinArrowShots(vector<vector<int>>& points) {
+    static bool compare(const vector<int>& x, const vector<int>& y)
+    {
+        return (x[0] < y[0]);
+    }
 
+    int findMinArrowShots(vector<vector<int>>& points) {
+        int pre;
+        int count;
+
+        sort(points.begin(), points.end(), compare);
+        pre = points[0][1];
+        count = 1;
+        for (int i = 1; i < points.size(); i++)
+        {
+            if (points[i][0] > pre)                                            // 不重叠
+            {
+                count++;
+                pre = points[i][1];
+            }
+            else
+            {
+                pre = min(pre, points[i][1]);                                  // 取最小值以解决区间的包含关系
+            }
+        }
+        
+        //printf("%d", pre);
+        return count;
     }
 };
 // @lc code=end
---
+++ 763.划分字母区间.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=763 lang=cpp
+ *
+ * [763] 划分字母区间
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+
+    }
+};
+// @lc code=end
+
