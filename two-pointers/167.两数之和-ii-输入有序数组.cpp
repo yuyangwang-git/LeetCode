c105c626cfb164074a105b40aafbe32ddf2d22c0
yuyangwang-git <mrwangyuyang@gmail.com>
2022-03-18 16:35:21 +0800

    add problem 167

--- two-pointers/167.两数之和-ii-输入有序数组.cpp
+++ two-pointers/167.两数之和-ii-输入有序数组.cpp
@@ -8,7 +8,30 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
+        int i;
+        int j;
+        int sum;
 
+        i = 0;
+        j = numbers.size() - 1;
+        while (i < j)
+        {
+            sum = numbers[i] + numbers[j];
+            if (sum < target)
+            {
+                i++;
+            }
+            else if (sum > target)
+            {
+                j--;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return vector<int> {i + 1, j + 1};
     }
 };
 // @lc code=end
